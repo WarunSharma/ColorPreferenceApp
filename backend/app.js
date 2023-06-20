3245a93ceaeaf80a1d7602118520945ce8ce92a6
Warun Sharma <warunsharma.95@gmail.com>
2023-06-20 22:22:26 +0530

    Initial Commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('./src/db/connect')
+const { createOrUpdateUserPreference, getUserPreference } = require('./src/controllers/userpreference');
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 3000;
+
+// CORS applied
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.json());
+
+// Routes
+app.get("/preference/:username", getUserPreference)
+app.post("/preference", createOrUpdateUserPreference)
+
+app.use((req, res) => {
+    res.status(200).json({});
+});
+
+app.listen(port, ()=>{
+    console.log("application running on port " + port);
+})
